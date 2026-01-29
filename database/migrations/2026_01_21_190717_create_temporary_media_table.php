b65e8ece0e8050440884bd498b2b03e40d14c466
Sahid Anwar <hay.sahid@gmail.com>
2026-01-29 16:37:11 +0700

    fix: nullable temporary sub folder

--- database/migrations/2026_01_21_190717_create_temporary_media_table.php
+++ database/migrations/2026_01_21_190717_create_temporary_media_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('temporary_media', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('store_id')->nullable();
-            $table->string('folder');
+            $table->string('folder')->nullable();
             $table->string('name');
             $table->string('file_name');
             $table->string('mime_type')->nullable();
