bfab5b23f6b3ce05c6627cfde24ebffcc7ce76a9
Sahid Anwar <hay.sahid@gmail.com>
2026-02-03 16:47:49 +0700

    feat: show order form field error

--- resources/js/Components/Chip.vue
+++ resources/js/Components/Chip.vue
@@ -28,7 +28,7 @@ const onClick = () => {
 <template>
     <button
         type="button"
-        class="flex items-center justify-center gap-2 cursor-pointer px-3.5 py-2 rounded-lg hover:bg-gray-50 outline outline-1 -outline-offset-1 outline-gray-400 min-w-[50px] text-center text-sm h-fit focus:ring-2 focus:ring-offset-2 focus:ring-primary-light"
+        class="flex items-center justify-center gap-2 cursor-pointer px-4 py-2 rounded-lg hover:bg-gray-50 outline outline-1 -outline-offset-1 outline-gray-400 min-w-[50px] text-center text-sm h-fit focus:ring-2 focus:ring-offset-2 focus:ring-primary-light"
         :class="{
             '!outline-2 !-outline-offset-2 !outline-primary bg-primary/10 hover:bg-primary/10':
                 props.selected,
